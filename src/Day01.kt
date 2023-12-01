425e30d9c88d856fea2104e0896259cfa718e700
Fazekas Levente <levente.fazekas@gmail.com>
2023-12-01 12:44:09 +0100

    Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,47 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val sum = input.sumOf { line ->
+            val digits = line.filter { it.isDigit() }
+            val number = digits.first().digitToInt() * 10 + digits.last().digitToInt()
+            number
+        }
+        return sum
     }
 
+    val digits = listOf(
+        "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+    )
+
     fun part2(input: List<String>): Int {
-        return input.size
+        val sum = input.sumOf {line ->
+            val firstDigitIndex = line.indexOfFirst { it.isDigit() }
+            val lastDigitIndex = line.indexOfLast { it.isDigit() }
+            val firstWriteOutDigitIndex = line.indexOfAny(digits)
+            val lastWriteOutDigitIndex = line.lastIndexOfAny(digits)
+
+            val first = if ((firstDigitIndex < firstWriteOutDigitIndex && firstDigitIndex != -1) || firstWriteOutDigitIndex == -1){
+                line[firstDigitIndex].digitToInt()
+            }else{
+                digits.indexOfFirst { it.startsWith(line.substring(firstWriteOutDigitIndex, firstWriteOutDigitIndex + 2)) } + 1
+            }
+
+            val last = if (lastDigitIndex > lastWriteOutDigitIndex || lastWriteOutDigitIndex == -1){
+                line[lastDigitIndex].digitToInt()
+            }else{
+                digits.indexOfFirst { it.startsWith(line.substring(lastWriteOutDigitIndex, lastWriteOutDigitIndex + 2)) } + 1
+            }
+
+            val number = first * 10 + last
+            number
+        }
+        return sum
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part2(testInput) == 281)
 
     val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    println(part1(input))
+    println(part2(input))
 }
